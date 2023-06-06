7af28d7e056e616dd7a01689ec80bf44dfd319d9
nikolastyl <nikostyliyis@gmail.com>
2023-06-06 11:34:25 +0300

    Initial commit

---
+++ src/main/java/com/example/yourdoctorsapp/YourDoctorsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.yourdoctorsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourDoctorsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourDoctorsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yourdoctorsapp/YourDoctorsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yourdoctorsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourDoctorsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
