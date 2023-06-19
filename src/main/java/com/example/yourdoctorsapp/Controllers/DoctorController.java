cb8c0a5722f2a1632b1b4a0d92561ae8534bfc3e
nikolastyl <nikostyliyis@gmail.com>
2023-06-19 16:40:43 +0300

    +++

--- src/main/java/com/example/yourdoctorsapp/Controllers/DoctorController.java
+++ src/main/java/com/example/yourdoctorsapp/Controllers/DoctorController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+//import com.example.yourdoctorsapp.SelectedOptions;
 import com.example.yourdoctorsapp.Repositories.DoctorRepository;
 
 @RestController
@@ -18,10 +19,15 @@ public class DoctorController {
     public List<String> getSpecialties() {
         return doctorRepository.findDistinctSpecialties();
     }
-
-    @GetMapping("/areas")
-    public List<String> getAreas() {
-        return doctorRepository.findDistinctAreas();
-    }
     
+    @GetMapping("/areas")
+    public List<String> getAreas(@RequestParam("specialty") String specialty) {
+        return doctorRepository.findDistinctAreasBySpecialty(specialty);}
+
+    @GetMapping("/docs")
+    public List<String> getDoctors(@RequestParam("specialty") String specialty, @RequestParam("area") String area) {
+        return doctorRepository.findTheDoctors(specialty, area);
+    }
+
+
 }
